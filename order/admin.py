da861ee6bdbd37da1ca2b45018f030896e0fd012
Pooja Giri <poojagiri@fb.com>
2020-10-21 15:59:06 +0530

    adding order module backend

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Order
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['date', 'title', 'value', 'discount', 'final_value', 'is_paid']
+
+    list_filter = ['date', 'title', 'value', 'discount', 'final_value', 'is_paid']
+    search_fields = ['title']
+    list_per_page = 50
+    fields = ['date', 'title', 'value', 'discount', 'final_value', 'is_paid']
+    readonly_fields = ['tag_final_value']
+
