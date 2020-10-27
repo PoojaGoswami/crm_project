4cf2580ba13ab2b41b186ef247d70b0315bd6c0c
Pooja Giri <poojagiri@fb.com>
2020-10-27 05:38:18 +0530

    removing token module from import

--- user/views.py
+++ user/views.py
@@ -12,7 +12,6 @@ from django.db import IntegrityError
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
-from .tokens import account_activation_token
 from django.template.loader import render_to_string
 
 
