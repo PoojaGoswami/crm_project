a069b41acb954326fe9f5e2b3bc54c987a1fb32e
Pooja Giri <poojagiri@fb.com>
2020-11-28 01:29:03 +0530

    added autocomplete in birth date field

--- user/forms.py
+++ user/forms.py
@@ -11,14 +11,11 @@ class SignUpForm(UserCreationForm):
         self.fields['username'].widget.attrs['placeholder'] = 'Username'
 
         for visible in self.visible_fields():
-            print('visible', visible.field.widget.attrs)
-            # if visible.field.widget.attrs == 'birth_date':
-            #     visible.field.widget.attrs['class'] = 'datepicker'
             visible.field.widget.attrs['class'] = 'form-control'
 
     # username = templates.CharField(max_length=50, required=False, help_text='Optional.', widget= templates.TextInput(attrs={'placeholder':'Name'}))
     # birth_date = forms.CharField(max_length=25, required=False, help_text='', widget=forms.TextInput(attrs={'placeholder':'Date of birth'}))
-    birth_date = forms.DateTimeField(input_formats=['%d/%m/%Y'], widget=FengyuanChenDatePickerInput(attrs={'placeholder':'Date of birth', 'class': 'datepicker'}))
+    birth_date = forms.DateTimeField(input_formats=['%d/%m/%Y'], widget=FengyuanChenDatePickerInput(attrs={'placeholder':'Date of birth', 'class': 'datepicker', 'autocomplete': 'off'}))
     athlete_code = forms.CharField(max_length=50, required=False, help_text='', widget=forms.TextInput(attrs={'placeholder':'Athlete Code'}))
     address = forms.CharField(max_length=150, required=False, help_text='', widget=forms.TextInput(attrs={'placeholder':'Address'}))
     mobile = forms.CharField(max_length=30, required=False, help_text='', widget=forms.TextInput(attrs={'placeholder':'Mobile No.'}))
